40229036de44222700fc08b73d346835b60d17be
Lucas Keune <lucas.keune@qurasoft.de>
2023-01-26 23:13:59 +0100

    Clean up caching code to allow for easier addition of caching methods

--- lib/open_weather_client/cache.rb
+++ lib/open_weather_client/cache.rb
@@ -8,50 +8,61 @@ module OpenWeatherClient
     end
 
     def get(lat:, lon:, time:)
-      key = cache_key(lat: lat, lon: lon, time: time)
+      key = cache_key(lat, lon, time)
       raise KeyError unless present?(key)
 
-      caching = OpenWeatherClient.configuration.caching
-
-      if caching == :memory
-        @memory_keys.delete(key)
-        @memory_keys << key
-        return @memory_cache[key]
+      case OpenWeatherClient.configuration.caching
+      when :memory
+        memory_get(key)
+      else
+        raise NotImplementedError
       end
-
-      nil
     end
 
     def store(lat:, lon:, data:, time:)
-      caching = OpenWeatherClient.configuration.caching
-      key = cache_key(lat: lat, lon: lon, time: time)
+      key = cache_key(lat, lon, time)
 
-      if caching == :memory
-        @memory_cache[key] = data
-        @memory_keys.delete(key)
-        @memory_keys << key
+      case OpenWeatherClient.configuration.caching
+      when :memory
+        memory_store(key, data)
+      else
+        data
+      end
+    end
 
-        if @memory_keys.count > OpenWeatherClient.configuration.max_memory_entries
-          @memory_cache.delete(@memory_keys.shift)
-        end
+    private
+
+    def cache_key(lat, lon, time)
+      "#{lat}_#{lon}_#{time.strftime('%Y-%m-%dT%H')}"
+    end
+
+    def memory_get(key)
+      @memory_keys.delete(key)
+      @memory_keys << key
+      @memory_cache[key]
+    end
+
+    def memory_store(key, data)
+      @memory_cache[key] = data
+      @memory_keys.delete(key)
+      @memory_keys << key
+
+      if @memory_keys.count > OpenWeatherClient.configuration.max_memory_entries
+        @memory_cache.delete(@memory_keys.shift)
       end
 
       data
     end
 
-    private
-
-    def cache_key(lat:, lon:, time:)
-      "#{lat}_#{lon}_#{time.strftime('%Y-%m-%dT%H')}"
-    end
-
     def present?(key)
-      caching = OpenWeatherClient.configuration.caching
-      return false if caching == :none
-
-      return @memory_keys.include?(key) if caching == :memory
-
-      false
+      case OpenWeatherClient.configuration.caching
+      when :none
+        false
+      when :memory
+        @memory_keys.include?(key)
+      else
+        false
+      end
     end
   end
 end
